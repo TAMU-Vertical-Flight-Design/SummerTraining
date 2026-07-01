de8ed06a0801e454b4a895e08c8efdd26b192fc0
Harshit-Saini0 <harshitsaini@tamu.edu>
2026-06-30 20:59:35 -0500

    added comment for usable candidate function`

--- afs/week4/mission_planner.py
+++ afs/week4/mission_planner.py
@@ -13,6 +13,7 @@ def choose_drop_zone(detections):
 def mission_summary():
     return f"pattern={SEARCH_PATTERN}, drop_altitude_m={DROP_ALTITUDE_M}, failsafe={FAILSAFE_MODE}"
 
+"""Return if drop zone is usable.""" 
 def is_candidate_usable(candidate): 
     if candidate is None: 
         return False 
